983f803fe4b0ed7e3f7be80612e13434d52152e7
Sidharth Rao <sidharthmrao@gmail.com>
2022-07-04 15:59:46 -0500

    Created History Section in UI TODO Add integration with local cron.

---
+++ scripts.js
@@ -0,0 +1,53 @@
+let events = [{
+    title: "asdf",
+    description: "this is a description",
+    time: "Jan 4th 6:00 PM UTC",
+    status: "SUCCESSFUL",
+    id: 1
+}, {title: "cron", description: "this is another description", time: "Jan 3rd 7:00 PM UTC", status: "FAILED", id: 0}
+
+]
+
+
+function component_event(event) {
+    let title = event.title;
+    let description = event.description;
+    let time = event.time
+    let status = event.status
+    let element = document.createElement("div")
+    element.classList.add("panel")
+    element.innerHTML = `
+        <h2>${title}</h2>
+        <h5>${time}</h5>
+        <h3>${description}</h3>
+        <h3 style=${(status === "FAILED") ? `"color: red;"` : `"color: limegreen;"`}>${status}</h3>
+    `
+    return element;
+}
+
+
+function start() {
+    let container = document.getElementById('event_list');
+    let sorted_events = events.sort(function (a, b) {
+        return b.id - a.id
+    })
+    for (let i = 0; i < sorted_events.length; i++) {
+        let element = component_event(sorted_events[i]);
+        container.appendChild(element);
+        console.log(element)
+    }
+}
+
+function populate(box_count) {
+    for (let i = 0; i < box_count; i++) {
+        console.log(i)
+    }
+}
+
+populate(5);
+
+function expand(id) {
+    let element = document.getElementById(id);
+    element.style.width = element.value.length + "ch";
+    console.log(element.style.width)
+}
